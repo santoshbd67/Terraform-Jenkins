219017f33dcbcf5af85fb15914b0b7f32adfb3ea
santoshbd67 <150413783+santoshbd67@users.noreply.github.com>
2024-07-22 01:36:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
     region = "us-east-1"  
 }
 
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
 resource "aws_instance" "foo" {
   ami           = "ami-04a81a99f5ec58529" 
   instance_type = "t2.micro"
